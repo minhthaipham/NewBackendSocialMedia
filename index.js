de7bcb166babb4b7aa79780bbf87abf1536ab987
AYKDM <minhthai23012002@gmail.com>
2023-10-09 15:20:15 +0800

    CORS *

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://social-network-ebon-alpha.vercel.app/",
+    // origin: "https://social-network-ebon-alpha.vercel.app/",
+    origin: "*",
     credentials: true,
   })
 );
